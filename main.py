4c9d585e11de2e95ed045e4607966d3d8a9f2799
akaeii__ <magpoc.joshuaray@gmail.com>
2024-11-27 22:26:20 +0800

    Refactored loose code into function-based structure.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("datasets/covid_data.csv")
 
-df = df[
-    [
-        "USMER",
+def read_data() -> pd.DataFrame:
+    df = pd.read_csv("datasets/covid_data.csv")
+
+    return df
+
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+
+    bool_columns = [
         "SEX",
         "PATIENT_TYPE",
-        "DATE_DIED",
         "INTUBED",
         "PNEUMONIA",
-        "AGE",
         "PREGNANT",
         "DIABETES",
         "COPD",
@@ -23,36 +26,44 @@ df = df[
         "OBESITY",
         "RENAL_CHRONIC",
         "TOBACCO",
-        "CLASIFFICATION_FINAL",
         "ICU",
     ]
-]
 
-print(df.head())
+    df = df[
+        [
+            "USMER",
+            "SEX",
+            "PATIENT_TYPE",
+            "DATE_DIED",
+            "INTUBED",
+            "PNEUMONIA",
+            "AGE",
+            "PREGNANT",
+            "DIABETES",
+            "COPD",
+            "ASTHMA",
+            "INMSUPR",
+            "HIPERTENSION",
+            "OTHER_DISEASE",
+            "CARDIOVASCULAR",
+            "OBESITY",
+            "RENAL_CHRONIC",
+            "TOBACCO",
+            "CLASIFFICATION_FINAL",
+            "ICU",
+        ]
+    ]
 
-bool_columns = [
-    "SEX",
-    "PATIENT_TYPE",
-    "INTUBED",
-    "PNEUMONIA",
-    "PREGNANT",
-    "DIABETES",
-    "COPD",
-    "ASTHMA",
-    "INMSUPR",
-    "HIPERTENSION",
-    "OTHER_DISEASE",
-    "CARDIOVASCULAR",
-    "OBESITY",
-    "RENAL_CHRONIC",
-    "TOBACCO",
-    "ICU",
-]
+    df.loc[:, bool_columns] = df.loc[:, bool_columns].replace(2, 0)
+    df.loc[:, bool_columns] = df.loc[:, bool_columns].replace(97, np.nan)
+    df.loc[:, bool_columns] = df.loc[:, bool_columns].replace(99, np.nan)
+    df.loc[:, "DATE_DIED"] = np.where(df.loc[:, "DATE_DIED"] == "9999-99-99", 0, 1)
 
-df[bool_columns] = df[bool_columns].replace(2, 0)
-df[bool_columns] = df[bool_columns].replace(97, np.nan)
-df[bool_columns] = df[bool_columns].replace(99, np.nan)
+    return df
 
-df["DATE_DIED"] = np.where(df["DATE_DIED"] == "9999-99-99", 0, 1)
 
-print(df.head(10))
+if __name__ == "__main__":
+    df = read_data()
+    df = preprocess_data(df)
+
+    print(df.head(10))
